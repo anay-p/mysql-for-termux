278ead4625508b114e6108b8b3e355f80f78a79b
Anay Pareek <59292344+TheLastAirbendr@users.noreply.github.com>
2020-09-20 23:47:02 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,13 +6,30 @@ apt upgrade
 shopt -s expand_aliases
 alias curtime="date +\"%d/%m/%Y %X: \""
 
-echo `curtime` "Checking to see if package MariaDB is installed..." > logs.log
+echo > logs.log
 
-dpkg -s $pkg &> /dev/null
+for alias in start setpass
+do
+	echo `curtime` "Checking to see if alias '${alias}' exists..." >> logs.log
+	response=`grep "${alias}=" /data/data/com.termux/files/usr/etc/bash.bashrc`
+	if [ ! -n "$response" ]
+	then
+		echo `curtime` "Alias '${alias}' does not exist" >> logs.log
+		echo `curtime` "Creating alias '${alias}'..." >> logs.log
+		echo -e "alias ${alias}=\"~\\${alias}.sh\"" >> /data/data/com.termux/files/usr/etc/bash.bashrc &&
+		echo `curtime` "Alias '${alias}' created successfully" >> logs.log ||
+		echo `curtime` "Failed to create alias '${alias}'" >> logs.log
+	else
+		echo `curtime` "Alias '${alias}' already exists" >> logs.log
+	fi
+done
+
+echo `curtime` "Checking to see if package 'MariaDB' is installed..." >> logs.log
+
+dpkg -s mariadb &> /dev/null
 if [ $? -eq 1 ]  # If package is not installed
 then
-
-        echo `curtime` "Package is not installed" >> logs.log
+        echo `curtime` "Package 'MariaDB' is not installed" >> logs.log
         echo `curtime` "Installing..." >> logs.log
         pkg install mariadb
 
@@ -20,23 +37,32 @@ then
 
         if [ $code -eq 100 ]  # If package was not found
         then
-                echo `curtime` "Could not locate package" >> logs.log
-                echo `curtime` "Package installation failed" >> logs.log
+                echo `curtime` "Could not locate package 'MariaDB'" >> logs.log
+                echo `curtime` "Failed to install package 'MariaDB'" >> logs.log
         elif [ $code -eq 0 ]  # If command executed successfully
         then
-                dpkg -s $pkg &> /dev/null
+                dpkg -s mariadb &> /dev/null
                 if [ $? -eq 0 ]
                 then
                         echo `curtime` "Installation complete" >> logs.log
                         mysql_upgrade
+			~/start.sh &&
+			echo `curtime` "MySQL server started successfully" >> logs.log ||
+			echo `curtime` "MySQL server failed to start" >> logs.log
                 else
-                        echo `curtime` "Package was not installed" >> logs.log
+			echo `curtime` "Package installation command executed but package 'MariaDB' was not installed" >> logs.log
                 fi
-                pkg clean
         else
                 echo `curtime` "Some unkown error occured during installation (code:${?})" >> logs.log
         fi
 
 else
-        echo `curtime` "Package is already installed" >> logs.log
+        echo `curtime` "Package 'MariaDB' is already installed" >> logs.log
+	mysql_upgrade
+	~/start.sh &&
+	echo `curtime` "MySQL server started successfully" >> logs.log ||
+	echo `curtime` "MySQL server failed to start" >> logs.log
 fi
+
+
+
