4237d0dc2a07f0b14f2d4cdc608399c3ca6ceac0
Anay Pareek <59292344+TheLastAirbendr@users.noreply.github.com>
2020-09-23 13:37:19 +0530

    Update setpass.sh

--- setpass.sh
+++ setpass.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-cond=""
+echo -n "New password: "
+read -s pass1
+echo -ne "\nRetype new password: "
+read -s pass2
+echo
 
-while [ "$cond" = "" ]
-do
-        echo -n "Enter your new password: "
-        read -s pass1
-        echo -ne "\nRe-enter your new password: "
-        read -s pass2
-        echo
-
-        if [ $pass1 = $pass2 ]
-        then
-                cond="set"
-        else
-                echo "The entered passwords do not match"
-                echo "Please try again"
-        fi
-done
-
-echo -e "alter user root@localhost identified by \"${pass1}\"" > setpass.sql
-mysql mysql < setpass.sql > mysqllogs.log
-rm setpass.sql
-
-if [ ! -s mysqllogs.log ]
+if [ ! $pass1 = $pass2 ]
 then
-        rm mysqllogs.log
+        echo "Sorry, passwords do not match."
+else
+        echo -e "alter user root@localhost identified by \"${pass1}\"" > setpass.sql
+        mysql mysql < setpass.sql > mysqllogs.log &&
+        echo "New password was successfully set." ||
+        echo "Failed to set new password"
+        rm setpass.sql
+        if [ ! -s mysqllogs.log ]
+        then
+                rm mysqllogs.log
+        fi
 fi
