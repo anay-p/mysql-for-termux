172414aca043ffae61b175801538d80494dae763
Anay Pareek <59292344+TheLastAirbendr@users.noreply.github.com>
2022-01-11 20:58:35 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,14 @@
+import pymysql as sql
+
+db = dql.connect(host="localhost", user="root", passwd="1234")
+cur = db.cursor()
+
+cur.execute("SHOW DATABASES;")
+
+result_table = cur.fetchall()
+for index, row in enumerate(result_table, start=1):
+    serial_number = str(index) + "."
+    print(serial_number, row[0])
+
+cur.close()
+db.close()
